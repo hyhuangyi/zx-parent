630b99721e4bca4b3e8fc0def6991f762183faf7
huangy <597505910@qq.com>
2019-11-25 16:58:12 +0800

    update

--- zx-biz-api/src/main/java/cn/biz/po/CdCity.java
+++ zx-biz-api/src/main/java/cn/biz/po/CdCity.java
@@ -6,13 +6,15 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import java.time.LocalDateTime;
 import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
@@ -48,10 +50,14 @@ public class CdCity implements Serializable {
     @ApiModelProperty(value = "省份code")
     private String provCode;
 
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @ApiModelProperty(value = "创建时间")
     @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;
 
+    @JsonSerialize(using = LocalDateTimeSerializer.class)//序列化
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)//反序列化
     @ApiModelProperty(value = "更新时间")
     @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;
