cfdd790e7b3ef71caafea27c23bbaa52f9eb4cdf
huangy <597505910@qq.com>
2019-11-26 16:07:11 +0800

    日志

--- zx-biz/src/main/java/cn/biz/service/SysLogServiceImpl.java
+++ zx-biz/src/main/java/cn/biz/service/SysLogServiceImpl.java
@@ -3,10 +3,12 @@ package cn.biz.service;
 import cn.biz.dto.OperateLogDTO;
 import cn.biz.mapper.SysOperateLogMapper;
 import cn.biz.po.SysOperateLog;
+import cn.common.util.string.StringUtils;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class SysLogServiceImpl implements ISysLogService {
@@ -20,8 +22,18 @@ public class SysLogServiceImpl implements ISysLogService {
      */
     @Override
     public IPage<SysOperateLog> getOperateLogList(OperateLogDTO dto) {
-        Page<SysOperateLog> page=new Page<>();
-        page.setRecords(sysOperateLogMapper.getOperateLogList(page,dto));
+        Page<SysOperateLog> page=new Page<>(dto.getCurrent(),dto.getSize());
+        List<SysOperateLog> list=sysOperateLogMapper.getOperateLogList(page,dto);
+        if(list.size()!=0){
+            list.forEach(l->{
+                String method=l.getOperateMethod();
+                if(!StringUtils.isBlank(method)){
+                    //获取倒数第二个点的位置
+                    l.setOperateMethod(method.substring(method.substring(0,method.lastIndexOf(".")).lastIndexOf(".")+1));
+                }
+            });
+        }
+        page.setRecords(list);
         return page;
     }
 }
