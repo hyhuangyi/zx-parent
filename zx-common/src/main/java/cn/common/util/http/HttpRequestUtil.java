76c5e5b958d744f195f18409351f02024a84937a
huangy <597505910@qq.com>
2020-01-07 16:55:51 +0800

    2020/1/3 update

--- zx-common/src/main/java/cn/common/util/http/HttpRequestUtil.java
+++ zx-common/src/main/java/cn/common/util/http/HttpRequestUtil.java
@@ -1,19 +1,25 @@
 package cn.common.util.http;
 
 import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicHeader;
-import java.io.BufferedReader;
+import org.apache.http.util.EntityUtils;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Map;
+import java.util.Set;
 
 public class HttpRequestUtil {
     private static CloseableHttpClient httpClient;
@@ -21,14 +27,19 @@ public class HttpRequestUtil {
     static {
         PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
         cm.setMaxTotal(100);
-        cm.setDefaultMaxPerRoute(20);
         cm.setDefaultMaxPerRoute(50);
         httpClient = HttpClients.custom().setConnectionManager(cm).build();
     }
 
+    /**
+     * json格式post请求
+     * @param url
+     * @param jsonString
+     * @param headerMap
+     * @return
+     */
     public static String post(String url, String jsonString, Map<String, String> headerMap) {
         CloseableHttpResponse response = null;
-        BufferedReader in = null;
         String result = "";
         try {
             HttpPost httpPost = new HttpPost(url);
@@ -42,15 +53,9 @@ public class HttpRequestUtil {
             }
             httpPost.setEntity(new StringEntity(jsonString, ContentType.create("application/json","utf-8")));
             response = httpClient.execute(httpPost);
-            in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-            StringBuffer sb = new StringBuffer("");
-            String line = "";
-            String NL = System.getProperty("line.separator");
-            while ((line = in.readLine()) != null) {
-                sb.append(line + NL);
-            }
-            in.close();
-            result = sb.toString();
+            HttpEntity entity = response.getEntity();
+            result = EntityUtils.toString(entity, "utf-8");
+            EntityUtils.consume(entity);
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -80,6 +85,50 @@ public class HttpRequestUtil {
             headers[i++] = new BasicHeader(entry.getKey(), entry.getValue());
         }
         return headers;
+    }
+
+    /**
+     * 表单上传文件
+     */
+    public static String uploadFile(File postFile, String postUrl, Map<String,String> postParam){
+        CloseableHttpResponse response=null;
+        String result = "";
+        try{
+            //把一个普通参数和文件上传给下面这个地址    是一个servlet
+            HttpPost httpPost = new HttpPost(postUrl);
+            //把文件转换成流对象FileBody
+            FileBody fundFileBin = new FileBody(postFile);
+            //设置传输参数
+            MultipartEntityBuilder multipartEntity = MultipartEntityBuilder.create();
+            multipartEntity.addPart("fileinfo", fundFileBin);//相当于<input type="file" name="media"/>
+            //设计文件以外的参数
+            Set<String> keySet = postParam.keySet();
+            for (String key : keySet) {
+                if(postParam.get(key)!=null){
+                    multipartEntity.addPart(key, new StringBody(postParam.get(key), ContentType.create("text/plain", "UTF-8")));
+                }
+            }
+            HttpEntity reqEntity =  multipartEntity.build();
+            httpPost.setEntity(reqEntity);
+            //发起请求   并返回请求的响应
+            response = httpClient.execute(httpPost);
+            HttpEntity entity = response.getEntity();
+            result = EntityUtils.toString(entity, "utf-8");
+            EntityUtils.consume(entity);
+        } catch (ClientProtocolException e1) {
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } finally{
+            try {
+                if (null != response) {
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
     }
 
 }
