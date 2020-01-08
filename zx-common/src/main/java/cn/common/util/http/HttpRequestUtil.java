ae06ecc89b431d34e99607a7f43aa68c29b12a1f
huangy <597505910@qq.com>
2020-01-08 15:31:54 +0800

    2020/1/8 update

--- zx-common/src/main/java/cn/common/util/http/HttpRequestUtil.java
+++ zx-common/src/main/java/cn/common/util/http/HttpRequestUtil.java
@@ -1,6 +1,8 @@
 package cn.common.util.http;
 
 import cn.common.exception.ZxException;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
@@ -13,7 +15,6 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -21,8 +22,11 @@ import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.springframework.web.multipart.MultipartFile;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -44,15 +48,15 @@ public class HttpRequestUtil {
      * @param headerMap
      * @return
      */
-    public static String get(String url, Map<String, String> headerMap) {
+    public static String get(String url, Map<String,String> req,Map<String, String> headerMap) {
         CloseableHttpResponse response = null;
         String result = "";
         try {
-            HttpGet httpGet = new HttpGet(url);
+            HttpGet httpGet = new HttpGet(url+getQueryString(req));
             RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(30000).setConnectionRequestTimeout(30000).setSocketTimeout(30000).build();
             httpGet.setConfig(requestConfig);
             httpGet.addHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8");
-            httpGet.setHeader("Accept", "application/json");
+            httpGet.setHeader("Accept", "*/*");
             Header[] headers = getHeaders(headerMap);
             if (headers.length != 0){
                 httpGet.setHeaders(headers);
@@ -90,7 +94,7 @@ public class HttpRequestUtil {
             RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(30000).setConnectionRequestTimeout(30000).setSocketTimeout(30000).build();
             httpPost.setConfig(requestConfig);
             httpPost.addHeader("Content-type", "application/json; charset=utf-8");
-            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Accept", "*/*");
             Header[] headers = getHeaders(headerMap);
             if (headers.length != 0){
                 httpPost.setHeaders(headers);
@@ -155,39 +159,27 @@ public class HttpRequestUtil {
         return result;
     }
 
-    private static void setPostParams(HttpPost httpost,Map<String, String> params) {
-        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
-        Set<String> keySet = params.keySet();
-        for (String key : keySet) {
-            nvps.add(new BasicNameValuePair(key, params.get(key).toString()));
-        }
-        try {
-            httpost.setEntity(new UrlEncodedFormEntity(nvps, "UTF-8"));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-    }
-
 
     /**
      * 表单上传文件
      */
-    public static String uploadFile(File postFile, String postUrl, Map postParam,Map headerMap){
-        CloseableHttpResponse response=null;
-        String result = "";
+    public static void uploadFile(MultipartFile file, String postUrl, Map postParam, Map headerMap, HttpServletResponse response){
+        CloseableHttpResponse closeableHttpResponse = null;
+        ServletOutputStream out = null;
         try{
             //把一个普通参数和文件上传给下面这个地址    是一个servlet
             HttpPost httpPost = new HttpPost(postUrl);
-            //把文件转换成流对象FileBody
-            FileBody fundFileBin = new FileBody(postFile);
+
             //设置传输参数
             MultipartEntityBuilder multipartEntity = MultipartEntityBuilder.create();
-            multipartEntity.addPart("fileinfo", fundFileBin);//相当于<input type="file" name="media"/>
+            multipartEntity.addBinaryBody("file",file.getInputStream(), ContentType.MULTIPART_FORM_DATA,"report");
             //设计文件以外的参数
-            Set<String> keySet = postParam.keySet();
-            for (String key : keySet) {
-                if(postParam.get(key)!=null){
-                    multipartEntity.addPart(key, new StringBody(postParam.get(key).toString(), ContentType.create("text/plain", "UTF-8")));
+            if (postParam!=null){
+                Set<String> keySet = postParam.keySet();
+                for (String key : keySet) {
+                    if(postParam.get(key)!=null){
+                        multipartEntity.addPart(key, new StringBody(postParam.get(key).toString(), ContentType.create("text/plain", "UTF-8")));
+                    }
                 }
             }
             HttpEntity reqEntity =  multipartEntity.build();
@@ -197,26 +189,73 @@ public class HttpRequestUtil {
                 httpPost.setHeaders(headers);
             }
             //发起请求   并返回请求的响应
-            response = httpClient.execute(httpPost);
-            HttpEntity entity = response.getEntity();
-            result = EntityUtils.toString(entity, "utf-8");
-            EntityUtils.consume(entity);
+            closeableHttpResponse = httpClient.execute(httpPost);
+            Header encode = closeableHttpResponse.getFirstHeader("Content-Type");  //请求头，要返回content-type，以便前台知道如何处理
+            response.setHeader(encode.getName(), encode.getValue());
+
+            HttpEntity entity = closeableHttpResponse.getEntity();  //取出返回体
+            out = response.getOutputStream();
+            entity.writeTo(out);  //将返回体通过响应流写到前台
+            out.flush();
         } catch (ClientProtocolException e1) {
             e1.printStackTrace();
         } catch (IOException e1) {
             e1.printStackTrace();
         } finally{
             try {
-                if (null != response) {
-                    response.close();
+                if (null != out) {
+                    out.close();
+                }
+                if(closeableHttpResponse!=null){
+                    closeableHttpResponse.close();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        return result;
     }
 
+    /**
+     * 下载
+     * @param url
+     * @param headerMap
+     * @return
+     */
+    public static void downloadFile(String url,Map<String,String> req, Map<String, String> headerMap,HttpServletResponse response) {
+        CloseableHttpResponse closeableHttpResponse = null;
+        ServletOutputStream out = null;
+        try {
+            HttpGet httpGet = new HttpGet(url+getQueryString(req));
+            RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(30000).setConnectionRequestTimeout(30000).setSocketTimeout(30000).build();
+            httpGet.setConfig(requestConfig);
+            httpGet.addHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8");
+            httpGet.setHeader("Accept", "*/*");
+            Header[] headers = getHeaders(headerMap);
+            if (headers.length != 0){
+                httpGet.setHeaders(headers);
+            }
+            closeableHttpResponse = httpClient.execute(httpGet);
+            Header encode = closeableHttpResponse.getFirstHeader("Content-Type");  //请求头，要返回content-type，以便前台知道如何处理
+            response.setHeader(encode.getName(), encode.getValue());
+            HttpEntity entity = closeableHttpResponse.getEntity();  //取出返回体
+            out = response.getOutputStream();
+            entity.writeTo(out);  //将返回体通过响应流写到前台
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+                if(closeableHttpResponse!=null){
+                    closeableHttpResponse.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     /**
      * 返回http请求头列表
@@ -235,23 +274,47 @@ public class HttpRequestUtil {
         return headers;
     }
 
-    public static void download(InputStream in, HttpServletResponse response) throws Exception{
-        OutputStream outputStream= response.getOutputStream();
+    /**
+     * 拼接参数
+     * @param paramMap
+     * @return
+     */
+    public static String getQueryString(Map<String, String> paramMap) {
+        String queryString = "";
+        if (MapUtils.isNotEmpty(paramMap)) {
+            boolean first = true;
+            for (String key : paramMap.keySet()) {
+                String value = paramMap.get(key);
+                if (StringUtils.isBlank(value)) {
+                    continue;
+                }
+                if (!first) {
+                    queryString += "&";
+                } else {
+                    queryString += "?";
+                    first = false;
+                }
+                queryString += key + "=";
+                try {
+                    queryString += URLEncoder.encode(value, "utf-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return queryString;
+    }
+
+    private static void setPostParams(HttpPost httpost,Map<String, String> params) {
+        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+        Set<String> keySet = params.keySet();
+        for (String key : keySet) {
+            nvps.add(new BasicNameValuePair(key, params.get(key).toString()));
+        }
         try {
-            response.setContentType("application/octet-stream;charset=UTF-8;");//设置返回的文件类型
-            response.addHeader("Content-disposition", "attachment;");// 设置头部信息
-            int bytesRead = 0;
-            byte[] buffer = new byte[8192];
-            while ( (bytesRead=in.read(buffer))!= -1) {
-                outputStream.write(buffer, 0, bytesRead);
-            }
-        } finally {
-            if(in!=null){
-                in.close();
-            }
-            if(outputStream!=null){
-                outputStream.close();
-            }
+            httpost.setEntity(new UrlEncodedFormEntity(nvps, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
         }
     }
 }
