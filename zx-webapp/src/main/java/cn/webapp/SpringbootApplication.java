5e3acb6b7fe6fea17beb5089028e08600bc9f663
huangy <597505910@qq.com>
2020-05-13 16:01:46 +0800

    2020/5/13

--- zx-webapp/src/main/java/cn/webapp/SpringbootApplication.java
+++ zx-webapp/src/main/java/cn/webapp/SpringbootApplication.java
@@ -5,10 +5,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -39,5 +43,15 @@ public class SpringbootApplication extends SpringBootServletInitializer {
       ApplicationContext context= SpringApplication.run(SpringbootApplication.class, args);
       CdCity city=(CdCity) context.getBean("city");
       System.out.println("code="+city.getCode()+";fullName="+city.getFullName());
+    }
+    /**
+     * 解决springBoot Invalid character found in the request target 特殊字符传参报错
+     * @return
+     */
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> connector.setProperty("relaxedQueryChars", "|{}[]\\"));
+        return factory;
     }
 }
