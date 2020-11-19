9e1e73ef40de433f246cb188989268960a54daaf
huangy <597505910@qq.com>
2020-11-19 15:52:28 +0800

    2020/11/19

--- zx-common/src/main/java/cn/common/consts/GuoRenEnum.java
+++ zx-common/src/main/java/cn/common/consts/GuoRenEnum.java
@@ -7,10 +7,11 @@ public enum GuoRenEnum {
     QS_CODE(0,"macd金叉&&布林突破&&连续3日上涨"),
     MACD_JINCHA( 1, "macd金叉"),
     THREE_UP( 2, "连续3日上涨"),
-    BULIN_TUPO( 3, "布林线突破上轨"),
+    BULIN_TUPO_SHANG( 3, "布林线突破上轨"),
     PE_MIN( 4, "市盈率最小"),
     DR_ZHANGTING( 5, "当日涨停股票"),
-    BANK_MIN( 6, "银行市净率最小");
+    BANK_MIN( 6, "银行市净率最小"),
+    BULIN_TUPO_XIA( 7, "布林线突破下轨");
 
     private int type;
     private String prefix;
