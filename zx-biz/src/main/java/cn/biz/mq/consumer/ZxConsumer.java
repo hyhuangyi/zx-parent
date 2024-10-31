14813b0da8bdf924da6584dc647a09df78a04f01
huangy <huangy@huonu.com>
2024-10-31 17:48:04 +0800

    11

--- zx-biz/src/main/java/cn/biz/mq/consumer/ZxConsumer.java
+++ zx-biz/src/main/java/cn/biz/mq/consumer/ZxConsumer.java
@@ -1,21 +1,21 @@
-package cn.biz.mq.consumer;
-
-import cn.common.consts.MqConst;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Component;
-
-@Component
-@Slf4j
-public class ZxConsumer {
-    @RabbitListener(queues = MqConst.QUEUE_NAME)
-    public void consumeMessage(Message message) {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            log.error("ZxConsumer 异常",e);
-        }
-       log.info(message.toString());
-    }
-}
+//package cn.biz.mq.consumer;
+//
+//import cn.common.consts.MqConst;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.amqp.core.Message;
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@Slf4j
+//public class ZxConsumer {
+//    @RabbitListener(queues = MqConst.QUEUE_NAME)
+//    public void consumeMessage(Message message) {
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            log.error("ZxConsumer 异常",e);
+//        }
+//       log.info(message.toString());
+//    }
+//}
--- zx-webapp/src/main/java/cn/webapp/controller/zx/RabbitMqController.java
+++ zx-webapp/src/main/java/cn/webapp/controller/zx/RabbitMqController.java
@@ -1,35 +1,35 @@
-package cn.webapp.controller.zx;
-
-import cn.common.consts.MqConst;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * Created by huangYi on 2018/9/12
- **/
-@Api(tags = "rabbitMq")
-@RestController
-@Slf4j
-public class RabbitMqController {
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
-
-    @RequestMapping(value = "/comm/rabbitMq",method = RequestMethod.POST)
-    @ApiOperation(value = "rabbitMq测试")
-    public Boolean sendMessage() {
-        new Thread(() -> {
-            for (int i = 0; i < 50; i++) {
-                rabbitTemplate.convertAndSend(MqConst.EXCHANGE_NAME, MqConst.ROUTING_KEY, i);
-            }
-        }).start();
-        log.info("全部完成");
-        return true;
-    }
-}
-
+//package cn.webapp.controller.zx;
+//
+//import cn.common.consts.MqConst;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.amqp.rabbit.core.RabbitTemplate;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * Created by huangYi on 2018/9/12
+// **/
+//@Api(tags = "rabbitMq")
+//@RestController
+//@Slf4j
+//public class RabbitMqController {
+//    @Autowired
+//    private RabbitTemplate rabbitTemplate;
+//
+//    @RequestMapping(value = "/comm/rabbitMq",method = RequestMethod.POST)
+//    @ApiOperation(value = "rabbitMq测试")
+//    public Boolean sendMessage() {
+//        new Thread(() -> {
+//            for (int i = 0; i < 50; i++) {
+//                rabbitTemplate.convertAndSend(MqConst.EXCHANGE_NAME, MqConst.ROUTING_KEY, i);
+//            }
+//        }).start();
+//        log.info("全部完成");
+//        return true;
+//    }
+//}
+//
